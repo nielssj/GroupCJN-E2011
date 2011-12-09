25d8f4e70975fd9c7b181ea121caf67c0be59c8b
Niels Soeholm <nm@9la.dk>
2011-12-09 18:01:37 +0100

    Added simple hashing of CPR numbers before barcode generation on voter cards.

---
+++ DigitalVoterList/DigitalVoterList/Central/Utility/BarCodeHashing.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BarCodeHashing.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DigitalVoterList.Central.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class BarCodeHashing
+    {
+        public static long Hash(uint cprno)
+        {
+            return Convert.ToInt64(cprno) * 17;
+        }
+
+        public static uint UnHash(long barcode)
+        {
+            return Convert.ToUInt32(barcode / 17);
+        }
+    }
+}
